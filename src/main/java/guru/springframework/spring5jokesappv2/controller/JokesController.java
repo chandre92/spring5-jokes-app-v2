5bbf27599a6effbbe051d3a47a1c70b491066e9e
chandre <disable1992@gmail.com>
2021-03-24 23:01:07 +0200

    Display jokes on ui. Closes #5

--- src/main/java/guru/springframework/spring5jokesappv2/controller/JokesController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/JokesController.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5jokesappv2.controller;
 import guru.springframework.spring5jokesappv2.service.JokesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +16,8 @@ public class JokesController {
     }
 
     @RequestMapping(path = {"/", ""})
-    public String getJoke() {
+    public String getJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
 
         return "index";
     }
